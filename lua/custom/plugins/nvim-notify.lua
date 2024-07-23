47a95689c10885c8482f764d26ad1773fb24db0f
Brandon Lim <brandonlim8890@gmail.com>
2024-07-22 20:59:51 -0400

    Update merge conflict

--- lua/custom/plugins/nvim-notify.lua
+++ lua/custom/plugins/nvim-notify.lua
@@ -1,16 +1,16 @@
 return {
-  "rcarriga/nvim-notify",
+  'rcarriga/nvim-notify',
   keys = {
     {
-      "<leader>un",
+      '<leader>un',
       function()
-        require("notify").dismiss({ silent = true, pending = true })
+        require('notify').dismiss { silent = true, pending = true }
       end,
-      desc = "Dismiss All Notifications",
+      desc = 'Dismiss All Notifications',
     },
   },
   opts = {
-    stages = "static",
+    stages = 'static',
     timeout = 3000,
     max_height = function()
       return math.floor(vim.o.lines * 0.75)
@@ -22,4 +22,12 @@ return {
       vim.api.nvim_win_set_config(win, { zindex = 100 })
     end,
   },
+  init = function()
+    -- when noice is not enabled, install notify on VeryLazy
+    -- if not LazyVim.has("noice.nvim") then
+    --   LazyVim.on_very_lazy(function()
+    --     vim.notify = require("notify")
+    --   end)
+    -- end
+  end,
 }
