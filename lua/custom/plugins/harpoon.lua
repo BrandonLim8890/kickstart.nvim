f488a95c635564948269ff6a8aac9acb7373367e
Tze Zhe Brandon Lim <brandonlim8890@gmail.com>
2024-07-23 10:13:47 -0400

    Create harpoon.lua

---
+++ lua/custom/plugins/harpoon.lua
@@ -0,0 +1,16 @@
+return {
+  "ThePrimeagen/harpoon",
+  lazy = false,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  config = true,
+  keys = {
+    { "<leader>A", "<cmd>lua require('harpoon.mark').add_file()<cr>",        desc = "Mark file with harpoon" },
+    { "<leader><c-h>", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>",          desc = "Go to next harpoon mark" },
+    { "<leader><c-j>", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>",          desc = "Go to next harpoon mark" },
+    { "<leader><c-k>", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>",          desc = "Go to next harpoon mark" },
+    { "<leader><c-l>", "<cmd>lua require('harpoon.ui').nav_file(4)<cr>",          desc = "Go to next harpoon mark" },
+    { "<leader>a", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Show harpoon marks" },
+  },
+}
