5a29ac3e2696ef1785756f82be1e94f2867b01da
Tze Zhe Brandon Lim <brandonlim8890@gmail.com>
2024-07-30 11:14:55 -0400

    Update harpoon.lua

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -7,10 +7,10 @@ return {
   config = true,
   keys = {
     { "<leader>A", "<cmd>lua require('harpoon.mark').add_file()<cr>",        desc = "Mark file with harpoon" },
-    { "<leader><c-h>", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>",          desc = "Go to next harpoon mark" },
-    { "<leader><c-j>", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>",          desc = "Go to next harpoon mark" },
-    { "<leader><c-k>", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>",          desc = "Go to next harpoon mark" },
-    { "<leader><c-l>", "<cmd>lua require('harpoon.ui').nav_file(4)<cr>",          desc = "Go to next harpoon mark" },
+    { "<leader><c-h>", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>",          desc = "Go to first harpoon mark" },
+    { "<leader><c-j>", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>",          desc = "Go to second harpoon mark" },
+    { "<leader><c-k>", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>",          desc = "Go to third harpoon mark" },
+    { "<leader><c-l>", "<cmd>lua require('harpoon.ui').nav_file(4)<cr>",          desc = "Go to forth harpoon mark" },
     { "<leader>a", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Show harpoon marks" },
   },
 }
