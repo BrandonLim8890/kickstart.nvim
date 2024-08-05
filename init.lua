561bc5678f7189aedec420ba6852504235d56fde
Brandon Lim <brandonlim8890@gmail.com>
2024-08-05 09:19:24 -0400

    Update harpoon

--- init.lua
+++ init.lua
@@ -381,7 +381,6 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-      vim.keymap.set('n', '<leader>sp', builtin.buffers, { desc = '[ ] Find existing buffers' })
       vim.keymap.set(
         'n',
         '<leader>sp',
--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,16 +1,90 @@
+local conf = require('telescope.config').values
+
+local function toggle_telescope(harpoon_files)
+  local finder = function()
+    local file_paths = {}
+    for _, item in ipairs(harpoon_files.items) do
+      table.insert(file_paths, item.value)
+    end
+
+    return require('telescope.finders').new_table {
+      results = file_paths,
+    }
+  end
+
+  require('telescope.pickers')
+    .new({}, {
+      prompt_title = 'Harpoon',
+      finder = finder(),
+      previewer = conf.file_previewer {},
+      sorter = conf.generic_sorter {},
+      attach_mappings = function(prompt_bufnr, map)
+        map('n', 'dd', function()
+          local state = require 'telescope.actions.state'
+          local selected_entry = state.get_selected_entry()
+          local current_picker = state.get_current_picker(prompt_bufnr)
+
+          table.remove(harpoon_files.items, selected_entry.index)
+          current_picker:refresh(finder())
+        end)
+        return true
+      end,
+    })
+    :find()
+end
+
 return {
-  "ThePrimeagen/harpoon",
-  lazy = false,
+  'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
   dependencies = {
-    "nvim-lua/plenary.nvim",
+    'nvim-telescope/telescope.nvim',
   },
-  config = true,
+  config = function()
+    require('telescope').load_extension 'harpoon'
+    -- require('harpoon'):setup()
+  end,
   keys = {
-    { "<leader>A", "<cmd>lua require('harpoon.mark').add_file()<cr>",        desc = "Mark file with harpoon" },
-    { "<leader><c-h>", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>",          desc = "Go to first harpoon mark" },
-    { "<leader><c-j>", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>",          desc = "Go to second harpoon mark" },
-    { "<leader><c-k>", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>",          desc = "Go to third harpoon mark" },
-    { "<leader><c-l>", "<cmd>lua require('harpoon.ui').nav_file(4)<cr>",          desc = "Go to forth harpoon mark" },
-    { "<leader>a", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Show harpoon marks" },
+    {
+      '<leader>a',
+      function()
+        require('harpoon'):list():add()
+      end,
+      desc = 'Harpoon File',
+    },
+    {
+      '<leader>sm',
+      function()
+        toggle_telescope(require('harpoon'):list())
+      end,
+      desc = '[S]earch Harpoon [M]arks',
+    },
+    {
+      '<leader><c-h>',
+      function()
+        require('harpoon'):list():select(1)
+      end,
+      desc = 'Go to first harpoon mark',
+    },
+    {
+      '<leader><c-j>',
+      function()
+        require('harpoon'):list():select(2)
+      end,
+      desc = 'Go to second harpoon mark',
+    },
+    {
+      '<leader><c-k>',
+      function()
+        require('harpoon'):list():select(3)
+      end,
+      desc = 'Go to third harpoon mark',
+    },
+    {
+      '<leader><c-l>',
+      function()
+        require('harpoon'):list():select(4)
+      end,
+      desc = 'Go to fourth harpoon mark',
+    },
   },
 }
