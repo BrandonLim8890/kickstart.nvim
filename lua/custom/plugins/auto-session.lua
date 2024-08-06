3c44a21afd19e851fe87f26c888f9eee667f50b5
Tze Zhe Brandon Lim <brandonlim8890@gmail.com>
2024-08-06 10:18:29 -0400

    Create auto-session.lua

---
+++ lua/custom/plugins/auto-session.lua
@@ -0,0 +1,15 @@
+return {
+  'rmagatti/auto-session',
+  config = function()
+    local auto_session = require 'auto-session'
+
+    auto_session.setup {
+      auto_restore_enabeld = false,
+      auto_session_suppress_dirs = { '~/', '~/Developer', '~/Downloads', '~/Documents', '~/Desktop' },
+    }
+
+    local keymap = vim.keymap
+
+    keymap.set('n', '<leader>wr', '<cmd>SessionRestore<CR>', { desc = '[W]orkspace [R]estore Sesssion' })
+  end,
+}
