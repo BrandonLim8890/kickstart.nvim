059b9be9fbb5e94668a949cb6f406daea72a87fc
Tze Zhe Brandon Lim <brandonlim8890@gmail.com>
2024-08-07 11:15:49 -0400

    Update auto-save.lua

--- lua/custom/plugins/auto-save.lua
+++ lua/custom/plugins/auto-save.lua
@@ -1,3 +1,12 @@
 return {
-  "pocco81/auto-save.nvim"
+  "pocco81/auto-save.nvim",
+  config = function()
+    require('auto-save').setup {
+      condition = function(buf)
+        if vim.bo[buf].filetype == 'harpoon' then
+          return false
+        end
+      end
+    }
+  end,
 }
