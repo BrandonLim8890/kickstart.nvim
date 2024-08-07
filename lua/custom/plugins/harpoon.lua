3dd6376c05a4180356d3e5c7df150e1b94ace635
Tze Zhe Brandon Lim <brandonlim8890@gmail.com>
2024-08-07 11:16:02 -0400

    Update harpoon.lua

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,43 +1,14 @@
-local conf = require('telescope.config').values
-
-local function toggle_telescope(harpoon_files)
-  local finder = function()
-    local file_paths = {}
-    for _, item in ipairs(harpoon_files.items) do
-      table.insert(file_paths, item.value)
-    end
-
-    return require('telescope.finders').new_table {
-      results = file_paths,
-    }
-  end
-
-  require('telescope.pickers')
-    .new({}, {
-      prompt_title = 'Harpoon',
-      finder = finder(),
-      previewer = conf.file_previewer {},
-      sorter = conf.generic_sorter {},
-      attach_mappings = function(prompt_bufnr, map)
-        map('n', 'dd', function()
-          local state = require 'telescope.actions.state'
-          local selected_entry = state.get_selected_entry()
-          local current_picker = state.get_current_picker(prompt_bufnr)
-
-          table.remove(harpoon_files.items, selected_entry.index)
-          current_picker:refresh(finder())
-        end)
-        return true
-      end,
-    })
-    :find()
-end
-
 return {
   'ThePrimeagen/harpoon',
   branch = 'harpoon2',
-  dependencies = {
-    'nvim-telescope/telescope.nvim',
+  ops = {
+    menu = {
+      width = vim.api.nvim_win_get_width(0) - 4,
+    },
+    settings = {
+      save_on_toggle = true,
+      sync_on_ui_close = true,
+    },
   },
   config = function()
     require('telescope').load_extension 'harpoon'
@@ -54,7 +25,8 @@ return {
     {
       '<leader>sm',
       function()
-        toggle_telescope(require('harpoon'):list())
+        local harpoon = require 'harpoon'
+        harpoon.ui:toggle_quick_menu(harpoon:list())
       end,
       desc = '[S]earch Harpoon [M]arks',
     },
