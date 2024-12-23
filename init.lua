447138d9079378da3993cc73fce4a4acd55bdfc9
Brandon Lim <brandonlim8890@gmail.com>
2024-12-23 13:11:35 +0700

    Remove illuminate and add indent line

--- init.lua
+++ init.lua
@@ -423,7 +423,8 @@ require('lazy').setup({
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
-      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+      vim.keymap.set('n', '<leader>sht', builtin.help_tags, { desc = '[S]earch [H]elp [T]ags' })
+      vim.keymap.set('n', '<leader>sh', '<cmd>Telescope find_files no_ignore=true<CR>', { desc = '[S]earch [H]idden' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
@@ -646,7 +647,7 @@ require('lazy').setup({
       local servers = {
         pyright = {},
         jdtls = {},
-        tsserver = {},
+        ts_ls = {},
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -4,6 +4,8 @@ return {
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help ibl`
     main = 'ibl',
-    opts = {},
+    opts = {
+      scope = { enabled = true, show_start = true, show_end = true, highlight = { 'Function', 'Label' } },
+    },
   },
 }
